5e0a60befc80274cf15d2f2762cf56d90603f0f0
naxim06082011 <nk06082011@gmail.com>
2024-08-28 20:28:18 +0300

    update task 21

--- task_21.py
+++ task_21.py
@@ -1,6 +1,10 @@
-kniga = 8192
-simvol =1
-vsego = kniga / simvol
-print(vsego)
+# Задача 21
+book_size = int(input("Введите размер книги в Кб: "))
+symbol_size = int(input("Введите размер символа в бит: "))
+bits_in_one_bytes = 8 # число бит в 1 байте
+bytes_in_kilobytes = 1024 # число байт в Кб
+
+symbol_count = book_size * bytes_in_kilobytes * bits_in_one_bytes / symbol_size
+print(symbol_count)
 
 
