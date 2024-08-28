6ce77ed2ea7f569980074e9f8aeee411e9db876b
naxim06082011 <nk06082011@gmail.com>
2024-08-28 21:43:23 +0300

    task_8.py

---
+++ task_28.py
@@ -0,0 +1,9 @@
+a = int(input('введите 1 число'))
+b = int(input('введите 2 число'))
+c = int(input('введите 3 число'))
+if a > b and a > c:
+    print('1 число самое большое')
+elif b > a and b > c:
+    print('2 число самое большое')
+elif c > a and c > b:
+    print('3 число самое большое')
