c4b35be4bb866f6f393c9c885b923bc3a8a6a4b5
naxim06082011 <nk06082011@gmail.com>
2024-08-28 20:39:46 +0300

    Update task_20

--- task_20.py
+++ task_20.py
@@ -1,4 +1,4 @@
-k = 10
-i = 10
-l = 12
-print(l)
+how_many_symbols = int(input('сколько символов в сообщении?'))
+bits_in_symbol = int(input('сколько весит один символ'))
+information = how_many_symbols * bits_in_symbol
+print(information)
