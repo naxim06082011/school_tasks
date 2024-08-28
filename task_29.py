d5fbe5e6c3c2eca2ba19ca0680d98715eed000c4
naxim06082011 <nk06082011@gmail.com>
2024-08-28 22:08:02 +0300

    task_29

---
+++ task_29.py
@@ -0,0 +1,20 @@
+a = int(input('введите 1 число'))
+b = int(input('введите 2 число'))
+if(not a%2):
+    f = a + b
+    print(f)
+elif(a%2):
+    y = a * b
+    print(y)
+else:
+    w = a - b
+    print(w)
+
+
+
+
+
+
+        
+
+
