62495c212ff3873e05da52efc2f43fbd60407a6a
naxim06082011 <nk06082011@gmail.com>
2024-08-29 20:38:26 +0300

    task_35

---
+++ task_35.py
@@ -0,0 +1,15 @@
+n = int(input('введите К-Н-Б(1-2-3)'))
+a = random.randint(1,3)
+if n = 1 and a = 3:
+    print('ты проиграл')
+elif n = 1 and a = 2:
+    print('ты выиграл')
+if n = 2 and a = 1:
+    print('ты проиграл')
+elif n = 2 and a = 3:
+    print('ты выиграл')
+if n = 3 and a = 1
+    print('ты проиграл')
+elif n = 3 and a = 1:
+    print('ты выиграл')
+
