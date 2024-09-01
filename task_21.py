52eae9015e5c80853ec117bfcd2fa6644911fd0f
naxim06082011 <nk06082011@gmail.com>
2024-09-01 18:32:24 +0300

    tasks

--- task_21.py
+++ task_21.py
@@ -1,6 +1,6 @@
 # Задача 21
 book_size = int(input("Введите размер книги в Кб: "))
-symbol_size = int(input("Введите размер символа в бит: "))
+symbol_size = int(input("Введите размер символа в бит: 1"))
 bits_in_one_bytes = 8 # число бит в 1 байте
 bytes_in_kilobytes = 1024 # число байт в Кб
 
--- task_39.py
+++ task_39.py
@@ -1,12 +1,3 @@
-# a = 1
-# b = 2
-# c = 3
-# s = a + b + c
-# f = 10 / 3
-# y = s * f
-# print(y)
-
-
 res = 0
 for i in range(1, 10):
     res += i
